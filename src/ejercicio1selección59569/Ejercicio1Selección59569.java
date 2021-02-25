ec16ace1bd815055281ee29726249eed5bdc6214
amigo <amigo@192.168.1.111>
2021-02-24 19:22:28 -0600

    Hola

---
+++ src/ejercicio1selección59569/Ejercicio1Selección59569.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1selección59569;
+
+/**
+ *
+ * @author amigo
+ */
+public class Ejercicio1Selección59569 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner datosUsuario = new Scanner(System.in);
+        
+        System.out.println("Introduce el primer número");
+        int numero1 = datosUsuario.nextInt();
+        
+        System.out.println("Introduce el segundo número");
+        int numero2 = datosUsuario.nextInt();
+        
+        System.out.println("Introduce el tercer número");
+        int numero3 = datosUsuario.nextInt();
+        
+        if(numero1 > numero2 && numero1 > numero3){
+        
+            System.out.println("El número " + numero1 + " es el mayor");
+                    
+        if(numero2 > numero1 && numero2 > numero3){
+        
+            System.out.println("El número " + numero2 + " es el mayor");
+                    
+        if(numero3 > numero1 && numero3 > numero2){
+        
+            System.out.println("El número " + numero3 + " es el mayor");            
+        }
+    }
+    
+}
